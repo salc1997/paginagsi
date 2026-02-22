df068a2e6ee33891c7ce91c22e70005292c13071
salc1997 <lezamasteve@gmail.com>
2026-02-22 15:12:30 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Función para mostrar el modal
+function abrirContacto() {
+    document.getElementById("modalContacto").style.display = "block";
+}
+
+// Función para ocultar el modal
+function cerrarContacto() {
+    document.getElementById("modalContacto").style.display = "none";
+}
+
+// Cerrar el modal si el usuario hace clic fuera de la caja blanca
+window.onclick = function(event) {
+    let modal = document.getElementById("modalContacto");
+    if (event.target == modal) {
+        cerrarContacto();
+    }
+}
